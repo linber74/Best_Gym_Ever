88c5eda4eb16ff780d1dfb8214a7d8fae65bf725
Linda Bergin <linber24@gmail.com>
2025-10-21 15:45:06 +0200

    made changes in Member

--- src/Sprint_2/Gym_Assignment2/Member.java
+++ src/Sprint_2/Gym_Assignment2/Member.java
@@ -4,20 +4,50 @@ import java.time.LocalDate;
 
 public class Member  {
 
-    private String name;
-    private String address;
-    private String personNr;
-    private LocalDate memberSince;
-    private LocalDate lastPayment;
+    private final String name;
+    private final String address;
+    private final String email;
+    private final String personNr;
+    private final LocalDate memberSince;
+    private final LocalDate lastPayment;
     private final MemberType memberType;
 
-    public Member(MemberType memberType, String name, String address, String personNr,
-                  LocalDate memberSince, LocalDate lastPayment) {
-        this.memberType = memberType;
+    public Member(String name, String address, String email, String personNr,
+                  LocalDate memberSince, LocalDate lastPayment, MemberType memberType) {
         this.name = name;
         this.address = address;
+        this.email = email;
         this.personNr = personNr;
         this.memberSince = memberSince;
         this.lastPayment = lastPayment;
+        this.memberType = memberType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPersonNr() {
+        return personNr;
+    }
+
+    public LocalDate getMemberSince() {
+        return memberSince;
+    }
+
+    public LocalDate getLastPayment() {
+        return lastPayment;
+    }
+
+    public MemberType getMemberType() {
+        return memberType;
     }
 }
