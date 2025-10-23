a4fb9f24fe439fa82a94e30dead81f77def89e13
Linda Bergin <linber24@gmail.com>
2025-10-23 17:43:44 +0200

    class PtLogger made with method for write to logg

---
+++ src/Assigment_2/PtLogger.java
@@ -0,0 +1,21 @@
+package Assigment_2;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+
+public class PtLogger {
+
+    public void log(Member member) {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("pt_log.txt", true))) {
+
+            writer.write(member.getName() + ";" + member.getPersonNr() + ";" + LocalDate.now());
+            writer.newLine();
+
+        } catch (IOException e) {
+            System.out.println("Fel! Gick inte spara besöket: " + e.getMessage());
+        }
+    }
+}
