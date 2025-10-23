ba74d35a0ef7a85fc2de54fb4a0a779316a92a36
Linda Bergin <linber24@gmail.com>
2025-10-23 18:09:13 +0200

    Created a class Gymsystem that coordinates everthing

---
+++ src/Assigment_2/GymSystem.java
@@ -0,0 +1,28 @@
+package Assigment_2;
+
+public class GymSystem {
+
+    private final MemberService memberService;
+    private final PtLogger log;
+
+    public GymSystem(MemberService memberService, PtLogger log) {
+        this.memberService = memberService;
+        this.log = log;
+    }
+
+    public void checkStatus(String input) {
+        Member member = memberService.findMember(input);
+
+        if (member == null){
+            System.out.println("OBehörig!");
+        }
+        else if (member.isCurrentMember()){
+            System.out.println("Medlem! (" + member.getMemberType().getFormattedMemberType());
+            System.out.println("Välkommen " + member.getName());
+            log.log(member);
+        }
+        else{
+            System.out.println("Före detta medlem!");
+        }
+    }
+}
