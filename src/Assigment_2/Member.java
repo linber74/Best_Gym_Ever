10dbe947bdb91f5150a2777af959ac7415362ae2
Linda Bergin <linber24@gmail.com>
2025-10-24 02:54:30 +0200

    Small change back in Member

--- src/Assigment_2/Member.java
+++ src/Assigment_2/Member.java
@@ -5,14 +5,20 @@ import java.time.LocalDate;
 public class Member  {
 
     private final String name;
+    private final String address;
+    private final String email;
     private final String personNr;
+    private final LocalDate memberSince;
     private final LocalDate lastPayment;
     private final MemberType memberType;
 
     public Member(String name, String address, String email, String personNr,
                   LocalDate memberSince, LocalDate lastPayment, MemberType memberType) {
         this.name = name;
+        this.address = address;
+        this.email = email;
         this.personNr = personNr;
+        this.memberSince = memberSince;
         this.lastPayment = lastPayment;
         this.memberType = memberType;
     }
